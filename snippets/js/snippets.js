f7c7ac833c56833174d5d630048c7e42c69c08cb
jboho <bohoj@mac.com>
2013-11-19 14:57:26 -0700

    Adding snippets folder for experimental ui forms

---
+++ snippets/js/snippets.js
@@ -0,0 +1,267 @@
+// working star ratings
+
+// Starrr plugin (https://github.com/dobtco/starrr)
+var __slice = [].slice;
+
+(function($, window) {
+  var Starrr;
+
+  Starrr = (function() {
+    Starrr.prototype.defaults = {
+      rating: void 0,
+      numStars: 5,
+      change: function(e, value) {}
+    };
+
+    function Starrr($el, options) {
+      var i, _, _ref,
+        _this = this;
+
+      this.options = $.extend({}, this.defaults, options);
+      this.$el = $el;
+      _ref = this.defaults;
+      for (i in _ref) {
+        _ = _ref[i];
+        if (this.$el.data(i) != null) {
+          this.options[i] = this.$el.data(i);
+        }
+      }
+      this.createStars();
+      this.syncRating();
+      this.$el.on('mouseover.starrr', 'span', function(e) {
+        return _this.syncRating(_this.$el.find('span').index(e.currentTarget) + 1);
+      });
+      this.$el.on('mouseout.starrr', function() {
+        return _this.syncRating();
+      });
+      this.$el.on('click.starrr', 'span', function(e) {
+        return _this.setRating(_this.$el.find('span').index(e.currentTarget) + 1);
+      });
+      this.$el.on('starrr:change', this.options.change);
+    }
+
+    Starrr.prototype.createStars = function() {
+      var _i, _ref, _results;
+
+      _results = [];
+      for (_i = 1, _ref = this.options.numStars; 1 <= _ref ? _i <= _ref : _i >= _ref; 1 <= _ref ? _i++ : _i--) {
+        _results.push(this.$el.append("<span class='glyphicon .glyphicon-star-empty'></span>"));
+      }
+      return _results;
+    };
+
+    Starrr.prototype.setRating = function(rating) {
+      if (this.options.rating === rating) {
+        rating = void 0;
+      }
+      this.options.rating = rating;
+      this.syncRating();
+      return this.$el.trigger('starrr:change', rating);
+    };
+
+    Starrr.prototype.syncRating = function(rating) {
+      var i, _i, _j, _ref;
+
+      rating || (rating = this.options.rating);
+      if (rating) {
+        for (i = _i = 0, _ref = rating - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; i = 0 <= _ref ? ++_i : --_i) {
+          this.$el.find('span').eq(i).removeClass('glyphicon-star-empty').addClass('glyphicon-star');
+        }
+      }
+      if (rating && rating < 5) {
+        for (i = _j = rating; rating <= 4 ? _j <= 4 : _j >= 4; i = rating <= 4 ? ++_j : --_j) {
+          this.$el.find('span').eq(i).removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+        }
+      }
+      if (!rating) {
+        return this.$el.find('span').removeClass('glyphicon-star').addClass('glyphicon-star-empty');
+      }
+    };
+
+    return Starrr;
+
+  })();
+  return $.fn.extend({
+    starrr: function() {
+      var args, option;
+
+      option = arguments[0], args = 2 <= arguments.length ? __slice.call(arguments, 1) : [];
+      return this.each(function() {
+        var data;
+
+        data = $(this).data('star-rating');
+        if (!data) {
+          $(this).data('star-rating', (data = new Starrr($(this), option)));
+        }
+        if (typeof option === 'string') {
+          return data[option].apply(data, args);
+        }
+      });
+    }
+  });
+})(window.jQuery, window);
+
+$(function() {
+  return $(".starrr").starrr();
+});
+
+$( document ).ready(function() {
+      
+  $('#stars').on('starrr:change', function(e, value){
+    $('#count').html(value);
+  });
+  
+  $('#stars-existing').on('starrr:change', function(e, value){
+    $('#count-existing').html(value);
+  });
+});
+
+
+// message-box
+/**
+ *
+ * jquery.charcounter.js version 1.2
+ * requires jQuery version 1.2 or higher
+ * Copyright (c) 2007 Tom Deater (http://www.tomdeater.com)
+ * Licensed under the MIT License:
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ */
+ 
+(function($) {
+    /**
+	 * attaches a character counter to each textarea element in the jQuery object
+	 * usage: $("#myTextArea").charCounter(max, settings);
+	 */
+	
+	$.fn.charCounter = function (max, settings) {
+		max = max || 100;
+		settings = $.extend({
+			container: "<span></span>",
+			classname: "charcounter",
+			format: "(%1 characters remaining)",
+			pulse: true,
+			delay: 0
+		}, settings);
+		var p, timeout;
+		
+		function count(el, container) {
+			el = $(el);
+			if (el.val().length > max) {
+			    el.val(el.val().substring(0, max));
+			    if (settings.pulse && !p) {
+			    	pulse(container, true);
+			    };
+			};
+			if (settings.delay > 0) {
+				if (timeout) {
+					window.clearTimeout(timeout);
+				}
+				timeout = window.setTimeout(function () {
+					container.html(settings.format.replace(/%1/, (max - el.val().length)));
+				}, settings.delay);
+			} else {
+				container.html(settings.format.replace(/%1/, (max - el.val().length)));
+			}
+		};
+		
+		function pulse(el, again) {
+			if (p) {
+				window.clearTimeout(p);
+				p = null;
+			};
+			el.animate({ opacity: 0.1 }, 100, function () {
+				$(this).animate({ opacity: 1.0 }, 100);
+			});
+			if (again) {
+				p = window.setTimeout(function () { pulse(el) }, 200);
+			};
+		};
+		
+		return this.each(function () {
+			var container;
+			if (!settings.container.match(/^<.+>$/)) {
+				// use existing element to hold counter message
+				container = $(settings.container);
+			} else {
+				// append element to hold counter message (clean up old element first)
+				$(this).next("." + settings.classname).remove();
+				container = $(settings.container)
+								.insertAfter(this)
+								.addClass(settings.classname);
+			}
+			$(this)
+				.unbind(".charCounter")
+				.bind("keydown.charCounter", function () { count(this, container); })
+				.bind("keypress.charCounter", function () { count(this, container); })
+				.bind("keyup.charCounter", function () { count(this, container); })
+				.bind("focus.charCounter", function () { count(this, container); })
+				.bind("mouseover.charCounter", function () { count(this, container); })
+				.bind("mouseout.charCounter", function () { count(this, container); })
+				.bind("paste.charCounter", function () { 
+					var me = this;
+					setTimeout(function () { count(me, container); }, 10);
+				});
+			if (this.addEventListener) {
+				this.addEventListener('input', function () { count(this, container); }, false);
+			};
+			count(this, container);
+		});
+	};
+
+})(jQuery);
+
+$(function() {
+    $(".counted").charCounter(320,{container: "#counter"});
+});
+
+
+// thumbnail carousel
+$(document).ready(function() {
+    $('#myCarousel').carousel({
+	    interval: 10000
+	})
+});
+
+// dynamic form fields
+$(document).ready(function(){
+    var next = 1;
+    $(".add-more").click(function(e){
+        e.preventDefault();
+        var addto = "#field" + next;
+        next = next + 1;
+        var newIn = '<br /><br /><input autocomplete="off" class="span3" id="field' + next + '" name="field' + next + '" type="text" data-provide="typeahead" data-items="8">';
+        var newInput = $(newIn);
+        $(addto).after(newInput);
+        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);  
+    });
+});
+
+
+
+// carousel-extended
+  jQuery(document).ready(function($) {
+ 
+        $('#myCarousel').carousel({
+                interval: 5000
+        });
+ 
+        $('#carousel-text').html($('#slide-content-0').html());
+ 
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click( function(){
+                var id_selector = $(this).attr("id");
+                var id = id_selector.substr(id_selector.length -1);
+                var id = parseInt(id);
+                $('#myCarousel').carousel(id);
+        });
+ 
+ 
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid', function (e) {
+                var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
+
