fdcff81e9e30b7f933a3ff0969dd1db14f3de580
jboho <bohoj@mac.com>
2014-03-11 14:13:51 -0600

    Document popover focus

--- docs/assets/js/application.js
+++ docs/assets/js/application.js
@@ -76,8 +76,12 @@
     })
 
     // popover demo
-    $('[data-toggle=popover]')
-      .popover()
+    $('.bs-docs-popover').popover()
+
+    // Popover dismiss on next click
+    $('.bs-docs-popover-dismiss').popover({
+      trigger: 'focus'
+    })
 
     // button state demo
     $('#loading-example-btn')
